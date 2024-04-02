63b03276b77f06ac2fa396c3b11cc16524c7a4cd
Laura McGowan <prime.lmcg@gmail.com>
2024-04-02 14:34:01 -0500

    transitionend eventListener and toggleActive function written and working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 const panels = document.querySelectorAll(".panel");
 
 function toggleOpen() {
+  console.log("Hello");
   this.classList.toggle("open");
 }
 
+function toggleActive(e) {
+  console.log(e.propertyName);
+  //this will check to see if flex or flex-grow has "fired"...if it has then add
+  // the .open-active css styling
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
 panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
